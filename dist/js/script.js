25739aec815056fba8d791555a7574527bf6c7ac
uuse <znrfire@gmail.com>
2020-11-27 16:12:52 +0400

    Media-queries

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,7 +1,8 @@
 window.addEventListener('DOMContentLoaded', () => {
     const menu = document.querySelector('.header__menu-list'),
     menuItem = document.querySelectorAll('.header__menu-item'),
-    hamburger = document.querySelector('.hamburger');
+    hamburger = document.querySelector('.hamburger'),
+    anchors = document.querySelectorAll('scroll');
 
     hamburger.addEventListener('click', () => {
         hamburger.classList.toggle('hamburger_active');
@@ -14,4 +15,32 @@ window.addEventListener('DOMContentLoaded', () => {
             menu.classList.toggle('header__menu-list_active');
         });
     });
+
+    anchors.forEach( item => {
+        item.addEventListener('click', (e) => {
+            e.preventDefault();
+
+            const test = document.getAttribute('href');
+
+            test.scrollIntoView ({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        });
+    });    
 });
+
+/* const anchors = document.querySelectorAll('a.scroll-to')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href')
+    
+    document.querySelector(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+} */
