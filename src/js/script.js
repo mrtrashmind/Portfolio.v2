a9a836424db67a5f4920e55e34d58520ed1de4cb
uuse <znrfire@gmail.com>
2021-01-02 16:49:55 +0400

    Добавлена страница доп. услуг, мелкие правки

--- src/js/script.js
+++ src/js/script.js
@@ -27,20 +27,68 @@ window.addEventListener('DOMContentLoaded', () => {
                 block: 'start'
             });
         });
-    });    
+    });
+
+/*
+    //Slider
+    let i = 1;
+    for(let li of slider.querySelectorAll('li')) {
+      li.style.position = 'relative';
+      li.insertAdjacentHTML('beforeend', `<span style="position:absolute;left:0;top:0">${i}</span>`);
+      i++;
+    }
+
+    //конфигурация
+    let width = 940; // ширина картинки
+    let count = 3; // видимое количество изображений
+
+    let list = slider.querySelector('ul');
+    let listElems = slider.querySelectorAll('li');
+
+    let position = 0; // положение ленты прокрутки
+
+    slider.querySelector('.portfolio__btn_prev').onclick = function() {
+        // сдвиг влево
+        position += width * count;
+        // последнее передвижение влево может быть не на 3, а на 2 или 1 элемент
+        position = Math.min(position, 0);
+        list.style.marginLeft = position + 'px';
+    };
+
+    slider.querySelector('.portfolio__btn_next').onclick = function() {
+        // сдвиг вправо
+        position -= width * count;
+        // последнее передвижение вправо может быть не на 3, а на 2 или 1 элемент
+        position = Math.max(position, -width * (listElems.length - count));
+        list.style.marginLeft = position + 'px';
+    };
+*/
 });
 
-/* const anchors = document.querySelectorAll('a.scroll-to')
 
-for (let anchor of anchors) {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault()
-    
-    const blockID = anchor.getAttribute('href')
-    
-    document.querySelector(blockID).scrollIntoView({
-      behavior: 'smooth',
-      block: 'start'
-    })
-  })
-} */
+//Отправка данных на сервер
+function send (event, php){
+    console.log("Отправка запроса");
+    event.preventDefault ? event.preventDefault() : event.returnValue = false;
+    var req = new XMLHttpRequest();
+    req.open('POST', php, true);
+    req.onload = function() {
+        if (req.status >= 200 && req.status < 400) {
+        json = JSON.parse(this.response); // Ебанный internet explorer 11
+            console.log(json);
+            
+            // ЗДЕСЬ УКАЗЫВАЕМ ДЕЙСТВИЯ В СЛУЧАЕ УСПЕХА ИЛИ НЕУДАЧИ
+            if (json.result == "success") {
+                // Если сообщение отправлено
+                alert("Сообщение отправлено");
+            } else {
+                // Если произошла ошибка
+                alert("Ошибка. Сообщение не отправлено");
+            }
+        // Если не удалось связаться с php файлом
+        } else {alert("Ошибка сервера. Номер: "+req.status);}}; 
+
+    // Если не удалось отправить запрос. Стоит блок на хостинге
+    req.onerror = function() {alert("Ошибка отправки запроса");};
+    req.send(new FormData(event.target));
+}
